09f6ca85ac4ff64f1c4cc74d02c82369df9161b7
ulkerozeren <53277896+ulkerozeren@users.noreply.github.com>
2019-09-19 20:21:57 +0300

    first

---
+++ State/Controllers/CookieController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace State.Controllers
+{
+    public class CookieController : Controller
+    {
+        public IActionResult Write()
+        {
+            CookieOptions cookieOptions = new CookieOptions()
+            {
+                 Expires=DateTime.Now.AddDays(1)
+            };
+
+            Response.Cookies.Append("test", "value: " + DateTime.Now, cookieOptions);
+
+            return View("Ortak");
+        }
+        public IActionResult Read()
+        {
+            string test=Request.Cookies["test"];
+            
+            return View("Ortak",test);
+        }
+        public IActionResult Delete()
+        {
+            Response.Cookies.Delete("test");
+
+            return View("Ortak");
+        }
+    }
+}
---
+++ State/Controllers/SessionController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace State.Controllers
+{
+    public class SessionController : Controller
+    {
+        public IActionResult Write()
+        {
+            HttpContext.Session.SetString("bilgeadam", "ba"+ DateTime.Now);
+            return View("Ortak");
+        }
+
+        public IActionResult Read()
+        {
+            string bilgeadam= HttpContext.Session.GetString("bilgeadam");
+            return View("Ortak",bilgeadam);
+        }
+
+        public IActionResult Delete()
+        {
+            HttpContext.Session.Remove("bilgeadam");
+            return View("Ortak");
+        }
+    }
+}
